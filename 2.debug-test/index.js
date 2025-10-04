f3b7c80d26e0a8addcf4086174c94aca191ebe94
yeyu98 <763525908@qq.com>
2025-10-04 17:04:32 +0800

    feat: nest debugger

---
+++ 2.debug-test/index.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: yeyu98
+ * @Date: 2025-10-04 16:46:01
+ * @LastEditors: yeyu98
+ * @LastEditTime: 2025-10-04 16:46:16
+ * @Description: 
+ */
+// nest debugger 参见.vscode/launch.json nest debug
+const os = require('os');
+
+const homedir = os.homedir();
+
+console.log(homedir);
